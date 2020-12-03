8369efa39706ad0d0267e2a1c8631479f881f9f2
Assembly13 <assemblyinstruction13@gmail.com>
2020-12-03 15:12:39 +0000

    revert

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,1 @@
-const React = require("react")
-const Layout = require("./src/components/global.css")
-
-// Logs when the client route changes
-exports.onRouteUpdate = ({ location, prevLocation }) => {
-  console.log("new pathname", location.pathname)
-  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
-}
-
-// Wraps every page in a component
-exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
+import "./src/styles/global.css";
