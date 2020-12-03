44cdb61a16b1dcc175c67dace64a9be70b42a04e
Assembly13 <assemblyinstruction13@gmail.com>
2020-12-03 15:08:57 +0000

    Change gatsby browser file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,13 @@
-import "./src/styles/global.css";
+const React = require("react")
+const Layout = require("./src/components/global.css")
+
+// Logs when the client route changes
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
+
+// Wraps every page in a component
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
